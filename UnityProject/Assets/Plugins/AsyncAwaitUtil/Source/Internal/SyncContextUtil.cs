463296069e1e2c63480f7062798dfc477772d37f
Ben Vandervalk <ben.vvalk@gmail.com>
2019-11-06 13:18:41 -0600

    SyncContextUtil: fix initialization in Edit Mode
    As described modesttree#9, using Unity3dAsyncAwaitUtil in
    Editor scripts causes an NPE, because `SyncContextUtil.Install` is never called
    to initialize global variables `SyncContextUtil.UnitySynchronizationContext` and
    `SyncContextUtil.UnityThreadId`.
    
    Fix this by adding the `[InitializeOnLoadMethod()]` attribute to the
    `SyncContextUtil.Install` method. Note that the existing
    `[RuntimeInitializeOnLoadMethod]` attribute is still needed in order for
    `SyncContextUtil.Install` to be correctly invoked in Play Mode.
    
    Fixes modesttree#9

--- UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
+++ UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
@@ -6,10 +6,17 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace UnityAsyncAwaitUtil
 {
     public static class SyncContextUtil
     {
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod()]
+#endif
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void Install()
         {
